464edc76fe0b40c6ab639e1335eb23bea8e4966e
Edward Loveall <edward@edwardloveall.com>
2017-05-05 23:07:36 -0400

    Add calendar when user signs up
    A user needs at least once calendar or they can't actually look at
    anything. This creates one for them when they sign up.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
     @user = sign_up(user_params)
 
     if @user.valid?
+      calendar = Calendar.create(title: 'My Campaign')
+      Permission.create(user: @user, calendar: calendar, level: 'owner')
       sign_in(@user)
       redirect_to root_path
     else
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@ FactoryGirl.define do
   factory :user do
     email 'user@example.com'
     password_digest 'password'
+
+    after(:create) do |user, _|
+      create(:calendar, users: [user])
+    end
   end
 end
