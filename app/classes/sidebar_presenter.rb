ca7dfa3c66639a89ff70de95eed6645b4ddadb00
Edward Loveall <edward@edwardloveall.com>
2017-09-21 13:45:35 -0400

    Add calendar_actions to the sidebar presenter
    Right now, this is just a link to create a new calendar and a link to
    see the list of calendars to edit them.
    
    use_html_5_tags has been added because I can't use the `nav` tag
    without it.

--- app/classes/sidebar_presenter.rb
+++ app/classes/sidebar_presenter.rb
@@ -1,5 +1,6 @@
 class SidebarPresenter < Keynote::Presenter
   presents :user
+  use_html_5_tags
 
   def calendar_list(for_current:)
     build_html do
@@ -10,6 +11,15 @@ class SidebarPresenter < Keynote::Presenter
             link_to calendar.title, calendar_path(calendar)
           end
         end
+      end
+    end
+  end
+
+  def calendar_actions
+    build_html do
+      nav class: 'calendar-actions' do
+        a.action t('helpers.action.calendar.new'), href: new_calendar_path
+        a.action t('helpers.action.calendar.index'), href: calendars_path
       end
     end
   end
--- spec/presenters/sidebar_presenter_spec.rb
+++ spec/presenters/sidebar_presenter_spec.rb
@@ -23,4 +23,20 @@ RSpec.describe SidebarPresenter do
       HTML
     end
   end
+
+  describe '#calendar_actions' do
+    it 'returns html for actions you can take on a calendar' do
+      user = double(:user)
+      presenter = present(:sidebar, user)
+
+      result = presenter.calendar_actions
+
+      expect(result).to eq <<-HTML.strip_heredoc.strip_html_whitespace
+        <nav class="calendar-actions">
+          <a class="action" href="/calendars/new">New Calendar</a>
+          <a class="action" href="/calendars">Edit Calendars</a>
+        </nav>
+      HTML
+    end
+  end
 end
