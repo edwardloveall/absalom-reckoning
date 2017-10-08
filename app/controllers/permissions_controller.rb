d591ffd71cb342bc8a1105df9b07a8af04952448
Edward Loveall <edward@edwardloveall.com>
2017-10-08 16:37:03 -0400

    Use Invitation ID when creating a new permission
    This felt more semantically correct to me

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -1,6 +1,6 @@
 class PermissionsController < AuthorizedController
   def create
-    invitation = Invitation.find(params[:invitation])
+    invitation = Invitation.find(params[:invitation_id])
     permission = Permission.new(params_from_invite(invitation))
 
     if permission.save
