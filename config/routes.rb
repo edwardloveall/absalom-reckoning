461d3ea8e2d3b5fabff5f3cea073406b48ad9acc
Edward Loveall <edward@edwardloveall.com>
2017-10-14 17:07:15 -0400

    Add 500 error page

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
     resources :invitations, only: [:create, :destroy]
   end
   match '/404', to: 'errors#not_found', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
   root to: 'welcome#show'
 end
