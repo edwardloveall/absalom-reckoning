89e12a2b95c18e734ba65b2d2688f34f0d82e746
Edward Loveall <edward@edwardloveall.com>
2017-11-04 11:56:43 -0400

    Move session_actions from UserPresenter to SidebarPresenter
    These actions only appear in the sidebar, so it made more sense to have
    this logic SidebarPresenter.

--- app/classes/sidebar_presenter.rb
+++ app/classes/sidebar_presenter.rb
@@ -22,4 +22,19 @@ class SidebarPresenter < Keynote::Presenter
       end
     end
   end
+
+  def session_actions
+    build_html do
+      ul class: :session do
+        li { link_to user.email, '' }
+        li do
+          link_to(
+            t('helpers.submit.session.destroy'),
+            session_path,
+            method: :delete
+          )
+        end
+      end
+    end
+  end
 end
--- spec/presenters/sidebar_presenter_spec.rb
+++ spec/presenters/sidebar_presenter_spec.rb
@@ -47,4 +47,20 @@ RSpec.describe SidebarPresenter do
       HTML
     end
   end
+
+  describe '#session_actions' do
+    it 'returns html for session related actions' do
+      user = create(:user)
+      presenter = present(:sidebar, user)
+
+      result = presenter.session_actions
+
+      expect(result).to eq <<-HTML.strip_heredoc.strip_html_whitespace
+        <ul class="session">
+          <li><a href="">#{user.email}</a></li>
+          <li><a rel="nofollow" data-method="delete" href="/session">Sign out</a></li>
+        </ul>
+      HTML
+    end
+  end
 end
