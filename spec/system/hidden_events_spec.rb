a3feed285c13e11f19ab73b829f7279024341432
Edward Loveall <edward@edwardloveall.com>
2017-12-07 21:03:58 -0500

    Editos can't affect event's hidden status

--- spec/system/hidden_events_spec.rb
+++ spec/system/hidden_events_spec.rb
@@ -1,29 +1,54 @@
 require 'rails_helper'
 
-RSpec.describe 'Owner creates a hidden event' do
-  it 'cannot be seen by an editor' do
-    owner = signed_up_user
-    editor = signed_up_user
-    calendar = owner.calendars.first
-    create(:permission, :editor, calendar: calendar, user: editor)
+RSpec.describe 'User creates a hidden event' do
+  context 'user is the calendar owner' do
+    it 'cannot be seen by an editor' do
+      owner = signed_up_user
+      editor = signed_up_user
+      calendar = owner.calendars.first
+      create(:permission, :editor, calendar: calendar, user: editor)
 
-    sign_in(owner)
-    visit calendar_path(calendar)
-    within('.week:nth-of-type(1) .date:nth-of-type(7)') do
-      click_on 'New Event'
+      sign_in(owner)
+      visit calendar_path(calendar)
+      within('.week:nth-of-type(1) .date:nth-of-type(7)') do
+        click_on 'New Event'
+      end
+      form_params = { title: 'TPK', hidden: true }
+      fill_form_and_submit(:event, form_params)
+
+      within('.week:nth-of-type(1) .date:nth-of-type(7)') do
+        expect(page).to have_css('li', text: 'TPK')
+      end
+
+      sign_in(editor)
+      visit calendar_path(calendar)
+
+      within('.week:nth-of-type(1) .date:nth-of-type(7)') do
+        expect(page).not_to have_css('li', text: 'TPK')
+      end
     end
-    form_params = { title: 'TPK', hidden: true }
-    fill_form_and_submit(:event, form_params)
+  end
 
-    within('.week:nth-of-type(1) .date:nth-of-type(7)') do
-      expect(page).to have_css('li', text: 'TPK')
+  context 'user is an editor' do
+    it 'can not see the hidden option' do
+      editor = signed_up_user
+      permission = create(:permission, :editor, user: editor)
+      calendar = permission.calendar
+
+      sign_in(editor)
+      visit calendar_path(calendar)
+      within('.week:nth-of-type(1) .date:nth-of-type(7)') do
+        click_on 'New Event'
+      end
+
+      within('form') do
+        expect(page).not_to have_css('input#event_hidden')
+      end
     end
+  end
+end
 
-    sign_in(editor)
-    visit calendar_path(calendar)
 
-    within('.week:nth-of-type(1) .date:nth-of-type(7)') do
-      expect(page).not_to have_css('li', text: 'TPK')
     end
   end
 end
